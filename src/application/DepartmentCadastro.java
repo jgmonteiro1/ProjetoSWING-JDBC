e63db8ece4a1d28c1a2d470c19497f962963d845
João Gabriel <jgmoreira15@gmail.com>
2019-10-26 16:00:30 -0300

    CONSEGUI FAZER A PROGRESS BAR FUNCIONAR, MESMO QUE NÃO USE DEIXA AI PRA SE LEMBRAR

--- src/application/DepartmentCadastro.java
+++ src/application/DepartmentCadastro.java
@@ -3,6 +3,10 @@ package application;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -11,13 +15,9 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import javax.swing.plaf.ProgressBarUI;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class DepartmentCadastro extends JDialog {
-
+	public static final long TEMPO = (100 * 60);
 	private final JPanel contentPanel = new JPanel();
 	private JTextField txtDigiteSeuNovo;
 	private JTextField textField;
@@ -39,65 +39,65 @@ public class DepartmentCadastro extends JDialog {
 	 * Create the dialog.
 	 */
 	public DepartmentCadastro() {
-		
+
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		JLabel lblNovoId = new JLabel("Novo ID");
 		lblNovoId.setBounds(10, 37, 46, 14);
 		contentPanel.add(lblNovoId);
-		
+
 		txtDigiteSeuNovo = new JTextField();
 		txtDigiteSeuNovo.setToolTipText("Digite o novo ID");
 		txtDigiteSeuNovo.setBounds(10, 62, 86, 20);
 		contentPanel.add(txtDigiteSeuNovo);
 		txtDigiteSeuNovo.setColumns(10);
-		
+
 		JLabel lblNovoDepartamento = new JLabel("Novo Departamento");
 		lblNovoDepartamento.setBounds(10, 103, 116, 27);
 		contentPanel.add(lblNovoDepartamento);
-		
+
 		textField = new JTextField();
 		textField.setBounds(10, 141, 86, 20);
 		contentPanel.add(textField);
 		textField.setColumns(10);
-		
-		
-		
+
 		JProgressBar progressBar = new JProgressBar();
 		progressBar.setBounds(10, 213, 414, 14);
 		contentPanel.add(progressBar);
 		progressBar.setStringPainted(true);
 		progressBar.setValue(0);
 		progressBar.setForeground(Color.GREEN);
-		
-		JButton btnInserir = new JButton("Inserir");
-		btnInserir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				while(progressBar.getValue()<100) {
-					progressBar.setValue(progressBar.getValue()+10);
-				}
+
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 				
+				Timer timer = new Timer();
+				TimerTask tarefa = new TimerTask() {
+			
+					@Override
+					public void run() {
+						progressBar.setValue(progressBar.getValue()+1);
+						
+					}
+				};
+			timer.scheduleAtFixedRate(tarefa, 50, 50);
 			}
+
 		});
-		btnInserir.setBounds(10, 179, 89, 23);
-		contentPanel.add(btnInserir);
-		
-		 		
+		btnNewButton.setBounds(10, 179, 89, 23);
+		contentPanel.add(btnNewButton);
+
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 		}
-		
-		
 
 	}
-	
-	
+
 }
-
-
